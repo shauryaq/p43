d7789c15c13676346e8c32652d275099a599e5fc
shauryaq <84259480+shauryaq@users.noreply.github.com>
2021-05-27 14:05:42 +0530

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,28 @@
+class createdrops{
+    constructor(x,y){
+        var option ={
+            'restitution':0.1,
+        'friction':0.01
+        }
+        this.body=Bodies.circle(x,y,5,option);
+        this.radius=5;
+        World.add(world,this.body)
+
+    }
+update(){
+    if(this.body.position.y>height){
+        Matter.Body.setPosition(this.body,{x:random(0,500),y:random(0,650)})
+    }
+}
+
+display(){
+    noStroke()
+    fill(0,0,255)
+    ellipseMode(RADIUS)
+    ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius)
+    
+}
+
+
+
+}
